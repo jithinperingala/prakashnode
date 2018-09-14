1435dbdf8964bf798874ff511b506a5175f7b5b7
jithinRaju <jithin.raju@iinerds.com>
2018-09-14 11:40:02 +0530

    sd

--- controllers/employees/createEmployee.js
+++ controllers/employees/createEmployee.js
@@ -5,12 +5,15 @@ pool = db.getPool();
 
 // Define routes handling profile requests
 router.post('/', function (req, res, next) {
+    try{
+
+    
     console.log("create Employee", req.body)
     var sqlQuery
     if (req.body.createUpdate == "1")
-        sqlQuery = 'CALL UpdateEmployee("' + req.body.employee_id + '","' + req.body.empName + '","' + req.body.aadarNumber + '","' + req.body.empType + '","c:/asdd/","' + req.body.address + '","' + req.body.insuranceNumber + '","' + req.body.contactno + '","1")';
+        sqlQuery = 'CALL UpdateEmployee("' + req.body.employee_id + '","' + req.body.empName + '",1,"' + req.body.aadarNumber + '","' + req.body.empType + '","' + "asdasd" + '","' + req.body.address + '","' + req.body.insuranceNumber + '","' + req.body.email + '","' + req.body.officialNumber + '","' + req.body.dob + '","' + req.body.gender + '","' + req.body.contactPerson + '","' + req.body.contactPersonMobile + '","' + req.body.contactPesonRelation + '","1","' + req.body.wagesType + '","' + req.body.salaryAmount + '","0+)';
     else
-        sqlQuery = 'CALL InsertEmployee("' + req.body.empName + '","' + req.body.aadarNumber + '","' + req.body.empType + '","c:/asdd/","' + req.body.address + '","' + req.body.insuranceNumber + '","' + req.body.contactno + '","1")';
+        sqlQuery = 'CALL InsertEmployee("' + req.body.empName + '",1,"' + req.body.aadarNumber + '","' + req.body.empType + '","' + "asdasd" + '","' + req.body.address + '","' + req.body.insuranceNumber + '","' + req.body.email + '","' + req.body.officialNumber + '","' + req.body.dob + '","' + req.body.gender + '","' + req.body.contactPerson + '","' + req.body.contactPersonMobile + '","' + req.body.contactPesonRelation + '","1","' + req.body.wagesType + '","' + req.body.salaryAmount + '","0+")';
     console.log(sqlQuery)
     pool.getConnection(function (err, connection) {
         // don't forget to check error
@@ -22,7 +25,10 @@ router.post('/', function (req, res, next) {
             res.send(result)
         })
     });
-
+}
+catch(ex){
+console.log(ex)
+}
 
 });
 module.exports = router
--- controllers/employees/searchEmployee.js
+++ controllers/employees/searchEmployee.js
@@ -12,7 +12,22 @@ router.get('/', function (req, res, next) {
         connection.query(sql, function (err, rows, fields) {
             if (err) throw err
 
-            // console.log('The solution is: ', rows[0].solution)
+             console.log('The solution is: ', rows[0])
+            connection.release()
+            //connection.release();
+            res.send(rows)
+        })
+    });
+});
+router.get('/empType', function (req, res, next) {
+    console.log("asdsad", req.query.emptype)
+    let sql = 'CALL SearchEmployee("' + req.query.emptype + '","' + req.query.key + '")'
+    pool.getConnection(function (err, connection) {
+        // don't forget to check error
+        connection.query(sql, function (err, rows, fields) {
+            if (err) throw err
+
+             console.log('The solution is: ', rows[0])
             connection.release()
             //connection.release();
             res.send(rows)
