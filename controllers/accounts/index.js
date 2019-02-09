7153cd4291d9e71cf963d8c57bebeac04486a3ec
jithin raju <jithinperingala@gmail.com>
2019-02-09 09:50:59 -0800

    sdfvv\

--- controllers/accounts/index.js
+++ controllers/accounts/index.js
@@ -2,6 +2,7 @@ var express = require('express')
   , router = express.Router()
 router.use('/payments', require('./payment'))
 router.use('/pettycashbook', require('./pettycashbook'))
+router.use('/bank', require('./bank'))
 
 router.get('/', function (req, res) {
   res.render('index')
