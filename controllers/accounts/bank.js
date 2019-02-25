6650d2577b2edbd38a018536ddcee6dbd0e4b86b
jithin raju <jithinperingala@gmail.com>
2019-02-24 18:34:50 -0800

    vendor bank account

--- controllers/accounts/bank.js
+++ controllers/accounts/bank.js
@@ -3,7 +3,7 @@ var express = require("express"),
   router = express.Router();
 
 // Define routes handling profile requests
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   try {
     console.log("as", req.query.siteID);
     console.log("as", req.query.searchKey);
@@ -13,10 +13,10 @@ router.get("/", function(req, res, next) {
       '","' +
       req.query.searchKey.replace(/[^0-9a-z]/gi, "") +
       '")';
-    pool.getConnection(function(err, connection) {
+    pool.getConnection(function (err, connection) {
       // don't forget to check error
 
-      connection.query(sql, function(err, rows, fields) {
+      connection.query(sql, function (err, rows, fields) {
         if (err) throw err;
 
         console.log("The solution is: ", rows[0].solution);
@@ -29,48 +29,56 @@ router.get("/", function(req, res, next) {
     res.send(ex);
   }
 });
-router.post("/", function(req, res, next) {
+router.post("/", function (req, res, next) {
+  console.log("bank", req.body);
   try {
     console.log("bdy", req.body);
-    var sqlQuery;
-    if (req.body.createUpdate == "1")
-      sqlQuery =
-        'CALL InsertContractor("' +
-        req.body.name +
-        '","' +
-        req.body.address +
-        '","' +
-        req.body.contactNo +
-        '","' +
-        req.body.mobile +
-        '","' +
-        req.body.email +
-        '","' +
-        req.body.contactPerson +
-        '",0,"10/11/2018")';
-    else
-      sqlQuery =
-        'CALL InsertContractor("' +
-        req.body.name +
-        '","' +
-        req.body.address +
-        '","' +
-        req.body.contactNo +
-        '","' +
-        req.body.mobile +
-        '","' +
-        req.body.email +
-        '","' +
-        req.body.contactPerson +
-        '",0,"10/11/2018")';
-    console.log(sqlQuery);
-    pool.getConnection(function(err, connection) {
-      connection.query(sqlQuery, function(err, result) {
-        if (err) throw err;
-        connection.release();
-        res.send(result);
+
+    for (var i = 0; i < req.body.data.length; i++) {
+      var sqlQuery;
+      if (req.body.createUpdate == "1")
+        sqlQuery =
+          'CALL InsertBankAccounts("' +
+          req.body.id +
+          '","' +
+          req.body.code +
+          '","' +
+          req.body.data[i].name +
+          '","' +
+          req.body.data[i].branch +
+          '","' +
+          req.body.data[i].ifsc +
+          '","' +
+          req.body.data[i].accNo +
+          '",0)';
+      else
+        sqlQuery =
+          'CALL InsertContractor("' +
+          req.body.id +
+          '","' +
+          req.body.code +
+          '","' +
+          req.body.data[i].name +
+          '","' +
+          req.body.data[i].branch +
+          '","' +
+          req.body.data[i].ifsc +
+          '","' +
+          req.body.data[i].accNo +
+          '",0)';
+      console.log(sqlQuery);
+      pool.getConnection(function (err, connection) {
+        connection.query(sqlQuery, function (err, result) {
+          if (err) throw err;
+          connection.release();
+          if (i == req.body.data.length - 1) {
+            res.send(result);
+          }
+
+        });
       });
-    });
+    }
+
   } catch (ex) {
     console.log(ex);
   }
--- controllers/vendor/index.js
+++ controllers/vendor/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
   , router = express.Router()
-router.use('/condractor', require('./contractor'))
+router.use('/contractor', require('./contractor'))
 router.use('/supplier', require('./supplier'))
 
 router.get('/', function (req, res) {
