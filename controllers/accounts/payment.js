192baeb9b69719cf705c6a5484ef82f6665b8827
jithin raju <jithinperingala@gmail.com>
2019-03-02 09:39:10 -0800

    payment

--- controllers/accounts/payment.js
+++ controllers/accounts/payment.js
@@ -5,7 +5,7 @@ var express = require('express')
 // Define routes handling profile requests
 router.get('/', function (req, res, next) {
     try {
-        
+
         console.log("as", req.query.siteID)
         console.log("as", req.query.searchKey)
         let sql = 'CALL GetEmployeetoAllocate("' + req.query.siteID + '","' + req.query.searchKey.replace(/[^0-9a-z]/gi, '') + '")'
@@ -15,17 +15,50 @@ router.get('/', function (req, res, next) {
             connection.query(sql, function (err, rows, fields) {
                 if (err) throw err
 
-                 console.log('The solution is: ', rows[0].solution)
+                console.log('The solution is: ', rows[0].solution)
                 connection.release()
 
                 res.send(rows)
             })
-            
+
         });
     }
     catch (ex) {
         res.send(ex)
     }
 
+});
+router.post("/savePayment", function (req, res, next) {
+
+    queryString =
+        'CALL InsertSupplierTransactions("' +
+        req.body.from.from +
+        '","' +
+        req.body.to.to +
+        '","' +
+        req.body.payment.amount +
+        '","' +
+        req.body.siteId +
+        '","' +
+        req.body.payment.cachTransferMode +
+        '","' +
+        req.body.from.fromAccount + '","' +
+        req.body.payment.date + '","' +
+        req.body.payment.description + '",0)';
+    console.log("bank", queryString);
+    try {
+        pool.getConnection(function (err, connection) {
+            // don't forget to check error
+
+            connection.query(queryString, function (err, rows, fields) {
+                if (err) throw err
+                connection.release()
+                res.send(rows)
+            })
+        });
+
+    } catch (ex) {
+        console.log(ex);
+    }
 });
 module.exports = router
